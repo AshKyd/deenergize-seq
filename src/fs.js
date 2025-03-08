7e6a832f5514e09ec5d665149d1a799881162fc2
Ash Kyd <ash@kyd.com.au>
2025-03-08 23:28:39 +1000

    bug: wrong path, testing on the fly

--- src/fs.js
+++ src/fs.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 import path from "path";
-import { __dirname } from "../utils.js";
+import { __dirname } from "./utils.js";
 
 export function read(file) {
   return fs.readFileSync(path.resolve(__dirname, "../", file));
