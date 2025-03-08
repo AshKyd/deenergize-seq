7a943f2a7b8789e69e583cd8a3a6711e64d364c2
Ash Kyd <ash@kyd.com.au>
2025-03-08 23:30:41 +1000

    bug: dirname should be relative to the root

--- src/fs.js
+++ src/fs.js
@@ -3,9 +3,9 @@ import path from "path";
 import { __dirname } from "./utils.js";
 
 export function read(file) {
-  return fs.readFileSync(path.resolve(__dirname, "../", file));
+  return fs.readFileSync(path.resolve(__dirname, file));
 }
 
 export function write(file, contents) {
-  return fs.writeFileSync(path.resolve(__dirname, "../", file), contents);
+  return fs.writeFileSync(path.resolve(__dirname, file), contents);
 }
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,8 @@
 import { fileURLToPath } from "url";
 import path from "path";
-export const __dirname = path.dirname(fileURLToPath(import.meta.url));
+export const __dirname = path.resolve(
+  path.dirname(fileURLToPath(import.meta.url)),
+  "../"
+);
+
+console.log({ __dirname });
